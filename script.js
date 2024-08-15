77a700dfc3784925bfddcadb75f8f4434af2eb76
shashank77t <110228304+shashank77t@users.noreply.github.com>
2024-08-15 16:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+//your JS code here. If requi.
+const age=document.getElementById("age");
+const name1=document.getElementById("name");
+const btn=document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+    const ageValidation = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age.value >= 18) {
+                resolve(name1.value);
+            } else {
+                reject(name1.value);
+            }
+        }, 4000);  // 4 seconds delay
+    });
+
+    ageValidation
+        .then((name) => {
+            alert(`Welcome, ${name1.value}. You can vote.`);
+        })
+        .catch((name) => {
+            alert(`Oh sorry, ${name1.value}. You aren't old enough.`);
+        });
+})
